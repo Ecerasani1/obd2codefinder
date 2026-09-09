df1b50551f1e3cf260fd1361af537abf98958295
e.cerasani <e.cerasani@agricolus.com>
2026-09-09 17:24:00 +0200

    wip

--- root_backend/obdCodeFinder/obdCodeFinder.Tests/obj/Debug/net10.0/obdCodeFinder.Tests.AssemblyInfo.cs
+++ root_backend/obdCodeFinder/obdCodeFinder.Tests/obj/Debug/net10.0/obdCodeFinder.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("obdCodeFinder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abca2ab441dd1e8387809f9f13edbeb2ab7e9d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4eeb338c176bbe2b08d5db92c52523fc4f8e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("obdCodeFinder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("obdCodeFinder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
